0fddcedc669c5ec674c9390f44952301770635bd
dani198912 <Feborcesim9>
2020-11-25 13:56:07 +0200

    Added scroll animationt, needs some fix

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,42 @@
-window.addEventListener(
-  "scroll",
-  () => {
-    console.log(window.pageYOffset);
-  },
-  false
-);
-const video = document.getElementsByClassName("video-to-scroll")[0];
+// window.addEventListener(
+//   "scroll",
+//   () => {
+//     console.log(window.pageYOffset);
+//   },
+//   false
+// );
 
-console.log("length: ", video);
-// function loop(){
-//   video.currentTime = window.pageYOffset/250
-//   window.requestAnimationFrame(loop)
+const videoSection = document.querySelector("#main-page");
+const video = videoSection.querySelector("video");
+//End Section
+const text = videoSection.querySelector(".logo");
+const end = document.querySelector("#despre");
 
-// }
-// window.requestAnimationFrame(loop)
+// const videoDurationInSeconds = await videoDuration();
+
+//Scroll Magic
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+  duration: 16000,
+  triggerElement: videoSection,
+  triggerHook: 0,
+})
+  .addIndicators()
+  .setPin(videoSection)
+  .addTo(controller);
+
+//Video animation
+
+const accelAmount = 0.15;
+let SCROLLPOS = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+  SCROLLPOS = e.scrollPos / 1000;
+});
 
 setInterval(() => {
-  video.currentTime = window.pageYOffset / 200;
-}, 33.33);
+  delay += (SCROLLPOS - delay ) * accelAmount;
+  
+  video.currentTime = delay;
+}, 1000/25);
