5c3254b2801cdcbedab3e23cb46f8fcd5706eabf
dani198912 <Feborcesim9>
2020-11-26 18:05:53 +0200

    Canvas size not working needs fix

--- scripts/imageSequence.js
+++ scripts/imageSequence.js
@@ -1,2 +1,16 @@
 const canvas = document.querySelector("canvas")
-canvas.getContext("2d")
+const ctx = canvas.getContext("2d")
+const img = new Image();
+const imageArray = [];
+const imgCount = 111;
+const pathToImage = ''
+const currentFrame = index =>{
+  return  `../images/sequence/mobile320/Ink/Ink_${index.toString().padStart(5, '0')}.jpg`;
+}
+img.src = currentFrame(40);
+console.log(img);
+console.log(ctx);
+img.addEventListener('load', e=>{
+  ctx.drawImage(img, 0, 0)
+  
+})
