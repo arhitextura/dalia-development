98f4560e4797e4f29ed2aacd08045912e03d88a3
dani198912 <Feborcesim9>
2020-12-01 13:01:48 +0200

    fixed canvas resize

--- scripts/imageSequence.js
+++ scripts/imageSequence.js
@@ -1,5 +1,6 @@
 'use strict'
 import { centerImageOffset } from './utils.js'
+const logoSection = document.querySelector(".logo-container")
 const canvas = document.querySelector("#image-sq-player")
 const ctx = canvas.getContext("2d")
 const img = new Image();
@@ -9,43 +10,42 @@ const pathToImage = ''
 const currentFrame = index => {
   return `../images/sequence/mobile320/Dalia-blooming/dalia_blooming_phone_414_812_${index.toString().padStart(5, '0')}.png`;
 }
+//init frame
+img.src = currentFrame(0);
+// canvas.onload = () => {
+//   canvas.width = window.innerWidth
+// }
 
 window.onload = () => {
   canvas.classList.add('loaded')
 }
 
-img.src = currentFrame(0);
-canvas.onload = () => {
-  canvas.width = window.innerWidth
-}
-
 function handleLoad() {
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-  const imgOffset = centerImageOffset(img.naturalWidth, img.naturalHeight)
-  ctx.drawImage(img, imgOffset.xOffset, imgOffset.yOffset)
+  drawImageActualSize();
 }
 function handleResize() {
-  console.log("Resize happemed");
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-  const imgOffset = centerImageOffset(img.naturalWidth, img.naturalHeight)
-
-  ctx.drawImage(img, imgOffset.xOffset, imgOffset.yOffset)
+  // canvas.width = window.innerWidth
+  // canvas.height = window.innerHeight
+  drawImageActualSize();
+}
+function drawImageActualSize () {
+  canvas.width = img.naturalWidth;
+  canvas.height = img.naturalHeight;
+  ctx.drawImage(img, 0,0, canvas.width, canvas.height)
 }
 
 img.addEventListener('load', handleLoad)
-
 window.addEventListener('resize', handleResize, false)
+
+//Animation
 const controller = new ScrollMagic.Controller();
 const scene = new ScrollMagic.Scene({
-  duration: 1500,
+  duration: 2500,
   triggerHook: 1,
 })
   // .addIndicators()
-  // .setPin(canvas)
+  .setPin(logoSection)
   .addTo(controller);
-
 
 const accelAmount = 0.15;
 let progress = 0;
@@ -53,17 +53,15 @@ let delay = 0;
 
 scene.on('progress', e => {
   progress = e.progress * 113
-  console.log(e);
-  if (e.progress >= 1.0) {
-    canvas.classList.remove('loaded');
+})
+scene.on("end", e => {
+  if(canvas.classList.contains('loaded')){
+    canvas.classList.remove('loaded')
   } else {
-    if (!canvas.classList.contains('loaded')) {
-      canvas.classList.add('loaded');
-    }
+    canvas.classList.add('loaded')
   }
 })
 window.setInterval(() => {
   delay += (progress - delay) * accelAmount;
-  // img.src = currentFrame(Math.floor(e.progress * 113))
   img.src = currentFrame(Math.floor(delay))
 }, 1000 / 25)
